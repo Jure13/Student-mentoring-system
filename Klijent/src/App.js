629372b947e961920b09a9c42ae3f70f7722e705
Jure Antunović <56198863+Jure13@users.noreply.github.com>
2022-04-04 13:31:09 +0200

    App.js u klijentu

---
+++ Klijent/src/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import {render} from "react-dom";
+//import SearchParams from "./SearchParams";
+import {Router} from "@reach/router";
+import Login from "./Login";
+import Logout from "./Logout";
+import Register from "./Register";
+import { UserContext } from "./UserContext";
+import Predmet from "./Predmet";
+
+
+
+
+const App = () => {
+
+    const [user, setUser] = useState("");
+
+    return(
+        <div>
+            <UserContext.Provider value={{user, setUser}}>
+                <Router>
+                    <Login path="/login" />
+                    <Predmet path="/predmet"/>
+                    {/* <Student path="/student"/>
+                    <Upisani path="/upisani/:email"/>
+                    <Uredi path="/uredi/:id"/> */}
+                    <Register path="/register" />
+                    <Logout path="/logout" />
+                </Router>
+            </UserContext.Provider>            
+        
+        </div>
+    );
+}
+
+
+
+render(<App/>, document.getElementById("root"));
